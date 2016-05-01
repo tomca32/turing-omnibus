c41216f90681d16822bc1891dded3edf438aa0c4
Tomislav Pesut <tomca32@gmail.com>
2016-05-01 19:05:20 -0400

    small refactor

--- 1-algorithms/main.js
+++ 1-algorithms/main.js
@@ -1,28 +1,31 @@
 (function () {
   "use strict";
 
+  function createCanvas() {
+    var canvas = document.createElement("canvas")
+    var cwAttribute = document.createAttribute("width");
+    cwAttribute.value = CANVAS_WIDTH + "";
 
-  function drawPixel(x, y) {
-    ctx.fillRect(x, y, 1, 1);
+    var chAttribute = document.createAttribute("height");
+    chAttribute.value = CANVAS_HEIGHT + "";
+    canvas.setAttributeNode(cwAttribute);
+    canvas.setAttributeNode(chAttribute);
+    document.body.appendChild(canvas);
+    return canvas;
+
+  }
+
+  function drawPixel(x, y, context) {
+    context.fillRect(x, y, 1, 1);
   }
 
   var x;
   var y;
   var c;
-  var CANVAS_WIDTH = "600";
-  var CANVAS_HEIGHT = "600";
+  var CANVAS_WIDTH = 600;
+  var CANVAS_HEIGHT = 600;
 
-  var canvas = document.createElement("canvas")
-
-  var cwAttribute = document.createAttribute("width");
-  cwAttribute.value = CANVAS_WIDTH;
-  var chAttribute = document.createAttribute("height");
-  chAttribute.value = CANVAS_HEIGHT;
-  canvas.setAttributeNode(cwAttribute);
-  canvas.setAttributeNode(chAttribute);
-  document.body.appendChild(canvas)
-  var ctx = canvas.getContext("2d");
-
+  var ctx = createCanvas().getContext("2d");
 
   var side = 30;
   for(var i = 0; i < CANVAS_HEIGHT; i++) {
@@ -31,7 +34,7 @@
       y = 5 + j * side/100;
       c = Math.round(x*x + y*y);
       if (c % 2 === 0) {
-        drawPixel(i, j);
+        drawPixel(i, j, ctx);
       }
     }
   }
