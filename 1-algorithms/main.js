92eb23b12ed2cc5482a37ac067bb9cfffe84404c
Tomislav Pesut <tomca32@gmail.com>
2016-05-02 17:16:37 -0400

    Refactored pixel drawing to use imageData - big perf boost

--- 1-algorithms/main.js
+++ 1-algorithms/main.js
@@ -15,27 +15,40 @@
 
   }
 
-  function drawPixel(x, y, context) {
-    context.fillRect(x, y, 1, 1);
+  function drawPixel(x, y, canvasWidth, canvasData) {
+    var index = (x + y * canvasWidth) * 4;
+
+    canvasData.data[index + 0] = 0;
+    canvasData.data[index + 1] = 0;
+    canvasData.data[index + 2] = 0;
+    canvasData.data[index + 3] = 255;
+  }
+
+  function updateCanvas(ctx, canvasData) {
+    console.log("UPDATING", canvasData);
+    ctx.putImageData(canvasData, 0, 0);
   }
 
   var x;
   var y;
   var c;
-  var CANVAS_WIDTH = 600;
-  var CANVAS_HEIGHT = 600;
+  var CANVAS_WIDTH = 1200;
+  var CANVAS_HEIGHT = 1200;
 
   var ctx = createCanvas().getContext("2d");
+  var canvasData = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  var side = 30;
+  var side = 48.6;
   for(var i = 0; i < CANVAS_HEIGHT; i++) {
     for(var j = 0; j < CANVAS_WIDTH; j++) {
-      x = 5 + i * side/100;
-      y = 5 + j * side/100;
+      x = i * side/100;
+      y = j * side/100;
       c = Math.round(x*x + y*y);
       if (c % 2 === 0) {
-        drawPixel(i, j, ctx);
+        drawPixel(i, j, CANVAS_WIDTH, canvasData);
       }
     }
   }
+
+  updateCanvas(ctx, canvasData)
 })();
